3a4a51a40b4598af8deadcd134857f0ee34c6947
Roger Kind Kristiansen <roger.kristiansen@gmail.com>
2013-09-19 17:06:11 +0200

    Added vcr filtering of passwords

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -2,4 +2,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   # Your HTTP request service. You can also use fakeweb, webmock, and more
   c.hook_into :webmock
+
+  c.filter_sensitive_data('<PASSWORD>') { '********' }
 end
