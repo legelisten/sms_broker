bb7d222e9bdc8ec895f7893e884e98c0eafa5221
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-07-20 14:07:02 +0300

    Add coveralls support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require 'spork'
 #require 'spork/ext/ruby-debug'
 
 Spork.prefork do
+  require 'coveralls'
+  Coveralls.wear!
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
@@ -89,8 +91,3 @@ end
 #
 # These instructions should self-destruct in 10 seconds.  If they don't, feel
 # free to delete them.
-
-
-
-
-
